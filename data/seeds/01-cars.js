e7cf05f59c717c4348355784afaee3f51227c950
Hunter5555 <hunter.m.allan@gmail.com>
2020-02-20 16:47:30 -0700

    added cars

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,13 +1,12 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('cars').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('cars').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {vin: 'ZA9RU31B9XLA12338', make: 'Lamborghini', model: 'Diablo', mileage: '15000', transmission: 'automatic', titlestatus: 'clean'},
+        {vin: 'WP0AC29945S692185', make: 'Porsche', model: '911', mileage: '18684', transmission: 'cvt', titlestatus: 'clean'},
+        {vin: 'WBAWC73578E067076', make: 'BMW', model: '3 Series', mileage: '15452', transmission: 'automatic', titlestatus: 'clean'}
       ]);
     });
 };
